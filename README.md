17c2730aa207ddeb80ea10052cdc0d5e6bfb27e0
UweHennig <uh@uwe-hennig.com>
2026-07-18 16:14:46 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Project Graph Generator
+
+This development generates generic graphs and serves as an example of practical solutions.
+
+#### Project status
+<br>18.7.2026 : Project created
---
+++ src/main/java/com/uwe_hennig/graph/generator/EmptyGraphGenerator.java
@@ -0,0 +1,15 @@
+/**
+ * @(#)EmptyGraphGenerator.java
+ * Copyright (c) 2026 Uwe Hennig
+ * All rights reserved.
+ */
+package com.uwe_hennig.graph.generator;
+
+/**
+ * EmptyGraphGenerator
+ *
+ * @author Uwe Hennig
+ */
+public class EmptyGraphGenerator {
+
+}
