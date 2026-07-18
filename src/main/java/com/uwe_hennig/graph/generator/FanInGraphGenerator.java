4635a3664afdb9a471276200df97e4d835203fd5
UweHennig <uh@uwe-hennig.com>
2026-07-18 22:08:34 +0200

    adding FanInGraphGenerator & FanOutGraphGenerator

---
+++ src/main/java/com/uwe_hennig/graph/generator/FanInGraphGenerator.java
@@ -0,0 +1,56 @@
+/**
+ * @(#)FanInGraphGenerator.java
+ * Copyright (c) 2026 Uwe Hennig
+ * All rights reserved.
+ */
+package com.uwe_hennig.graph.generator;
+
+import java.util.List;
+
+import com.uwe_hennig.graph.generator.contracts.Edge;
+import com.uwe_hennig.graph.generator.contracts.Graph;
+import com.uwe_hennig.graph.generator.contracts.GraphContext;
+import com.uwe_hennig.graph.generator.contracts.GraphGenerator;
+import com.uwe_hennig.graph.generator.contracts.SelectionRule;
+
+/**
+ * FanInGraphGenerator
+ *
+ * @author Uwe Hennig
+ */
+public class FanInGraphGenerator implements GraphGenerator {
+    private final GraphContext context;
+    private final SelectionRule selectionRule;
+
+    public FanInGraphGenerator(GraphContext context, SelectionRule selectionRule) {
+        this.context = context;
+        this.selectionRule = selectionRule;
+    }
+
+    @Override
+    public List<Graph> generate(int iterations, Graph initialGraph) {
+        if (initialGraph == null) {
+            return List.of();
+        }
+
+        List<Edge> filteredEdges = selectionRule != null ? selectionRule.apply(initialGraph.edges()) : initialGraph.edges();
+        if (filteredEdges.isEmpty()) {
+            return List.of();
+        }
+
+        Graph graph = Graph.of(context);
+        int endNodeId = context.nextNodeId();
+
+        for (Edge edge : initialGraph.edges()) {
+            if (!context.isUsedEdge(edge.edgeId())) {
+                int node = edge.nodeToId();
+                Edge newEdge = new Edge(context.nextEdgeId(), node, endNodeId);
+                graph.addEdge(newEdge);
+                context.setUsedEdge(edge.edgeId());
+            }
+        }
+
+        return List.of(graph);
+    }
+
+}
---
+++ src/main/java/com/uwe_hennig/graph/generator/FanOutGraphGenerator.java
@@ -0,0 +1,64 @@
+/**
+ * @(#)FanOutGraphGenerator.java
+ * Copyright (c) 2026 Uwe Hennig
+ * All rights reserved.
+ */
+package com.uwe_hennig.graph.generator;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.uwe_hennig.graph.generator.contracts.Edge;
+import com.uwe_hennig.graph.generator.contracts.Graph;
+import com.uwe_hennig.graph.generator.contracts.GraphContext;
+import com.uwe_hennig.graph.generator.contracts.GraphGenerator;
+import com.uwe_hennig.graph.generator.contracts.SelectionRule;
+
+/**
+ * FanOutGraphGenerator
+ *
+ * @author Uwe Hennig
+ */
+public class FanOutGraphGenerator  implements GraphGenerator {
+    private final GraphContext context;
+    private final int outNodeSize;
+    private final SelectionRule selectionRule;
+
+    public FanOutGraphGenerator(GraphContext context, SelectionRule selectionRule, int outNodeSize) {
+        this.context = context;
+        this.outNodeSize = outNodeSize;
+        this.selectionRule = selectionRule;
+    }
+
+    @Override
+    public List<Graph> generate(int iterations, Graph initialGraph) {
+        List<Graph> result = new ArrayList<>();
+
+        if (initialGraph == null || initialGraph.edges().isEmpty()) {
+            int startNode = context.nextNodeId();
+            Graph graph = Graph.of(context);
+            for (int i = 0; i < outNodeSize; i++) {
+                Edge newEdge = new Edge(context.nextEdgeId(), startNode, context.nextNodeId());
+                graph.addEdge(newEdge);
+            }
+            result.add(graph);
+            return result;
+        }
+
+        List<Edge> filteredEdges = selectionRule != null ? selectionRule.apply(initialGraph.edges()) : initialGraph.edges();
+        for (Edge edge : filteredEdges) {
+            context.setUsedEdge(edge.edgeId());
+
+            int startNode = edge.nodeToId();
+            Graph graph = Graph.of(context);
+            for (int i = 0; i < outNodeSize; i++) {
+                Edge newEdge = new Edge(context.nextEdgeId(), startNode, context.nextNodeId());
+                graph.addEdge(newEdge);
+            }
+            result.add(graph);
+        }
+
+        return result;
+    }
+
+}
